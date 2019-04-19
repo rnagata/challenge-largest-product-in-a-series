dcbc52e63a41069f474a1e638ad5d29fda7e274d
Ronald J Nagata Jr <junzo@Ronalds-MBP.home>
2019-04-19 10:33:41 -1000

    some code

--- largestProduct.js
+++ largestProduct.js
@@ -1,6 +1,41 @@
+'use strict';
+
 class LargestProduct {
   constructor(number) {
+    this.myNum = number;
+    this.highest = {
+      product : 0,
+      position : 0,
+    };
+  }
 
+  getLargestProductPosition(){
+    //returns the position of the largest product of 5 consecutive numbers found in the 1000-digit number. 
+    //count to the fifth position.
+    
+    let temp = this.myNum.split('');
+    let factors = [];
+    let products = [];
+    temp.forEach((member, index) => {
+      factors.push(member);
+      if (factors.length === 5){
+        let product = 1;
+        factors.forEach((member) => {
+          product *= parseFloat(member);
+        });
+        products.push(product);
+        }
+      // if (factors.length > 5){
+      //   factors.shift();
+      //}
+    });
+    console.log(products.length);
+   
+  }
+
+  getLargestProduct(){
+    
+    //returns the value of the largest product of 5 consecutive numbers found in the 1000-digit number.
   }
 }
 
